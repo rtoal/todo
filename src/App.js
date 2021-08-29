ca5547e03828655caf69c5f9f2ed5f0c02a0946f
Ray Toal <rtoal@lmu.edu>
2021-08-29 11:14:21 -0700

    Add the delete functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App">Things To Do</div>;
-}
+import { useState } from "react";
+import TodoList from "./TodoList";
 
-export default App;
+export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, content: "Meet Masao for Udon" },
+    { id: 2, content: "Fix up these horrible React notes" },
+  ]);
+  function deleteTodo(id) {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }
+  return (
+    <div className="App">
+      <h1>Things To Do</h1>
+      <TodoList todos={todos} deleter={deleteTodo} />
+    </div>
+  );
+}
