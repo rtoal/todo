10f84a95b707920347f89f01a2cd975be3c4ffdd
Ray Toal <rtoal@lmu.edu>
2021-08-29 11:17:39 -0700

    Entry form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import TodoList from "./TodoList";
+import TodoEntryForm from "./TodoEntryForm";
 
 export default function App() {
   const [todos, setTodos] = useState([
     { id: 1, content: "Meet Masao for Udon" },
     { id: 2, content: "Fix up these horrible React notes" },
   ]);
+  function addTodo(todo) {
+    setTodos((todos) => [...todos, todo]);
+  }
   function deleteTodo(id) {
     setTodos((todos) => todos.filter((todo) => todo.id !== id));
   }
@@ -13,6 +17,7 @@ export default function App() {
     <div className="App">
       <h1>Things To Do</h1>
       <TodoList todos={todos} deleter={deleteTodo} />
+      <TodoEntryForm adder={addTodo} />
     </div>
   );
 }
